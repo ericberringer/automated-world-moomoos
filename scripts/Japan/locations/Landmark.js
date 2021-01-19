4940905de0a937d85f5c2c10eedc62da76bd7350
jashah46 <jatin.shah01@gmail.com>
2021-01-19 16:05:03 -0600

    Added landmark-data-provider

---
+++ scripts/Japan/locations/Landmark.js
@@ -0,0 +1,7 @@
+export const Landmark = (landmark) => {
+    return `
+    <img class="countyFactsImage" src="${landmark.image}">
+        <h3 class="landmarks countryArticles">${landmark.name}</h3>
+  `
+}
+
--- scripts/Japan/main.js
+++ scripts/Japan/main.js
@@ -1,6 +1,9 @@
 import {useCity} from './cities/CityDataProvider.js'
 import {cityList} from './cities/CityList.js'
 
+
+
+
 // useCity()
 cityList()
 // console.log(cityList())
