96372385bba1a5a56adf1ea73d186896d33bbe69
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-19 15:39:26 -0600

    Created Data provider for landmarks

--- scripts/Iceland/main.js
+++ scripts/Iceland/main.js
@@ -6,3 +6,8 @@ cityList()
 // import {useCity} from './cities/CityDataProvider.js'
 
 // console.log(useCity())
+
+// import {useLandmark} from './locations/landmarkDataProvider.js'
+
+// useLandmark()
+// console.log(useLandmark())
