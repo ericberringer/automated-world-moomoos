446b9a364a4719ab75a2f4e7d56c93f45860b6e8
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-01-19 14:11:55 -0600

    fix div class and image

--- scripts/Netherlands/cities/City.js
+++ scripts/Netherlands/cities/City.js
@@ -1,7 +1,5 @@
 export const City = (city) => {
     return `
-    <div class="cities">
-    <p class="city__image">${city.image}</p>
     <p class="city__name">${city.name}</p>
     <img src="${city.image}" alt="cityImage" "class=cityImg" width="100px" height="100px">
     `
