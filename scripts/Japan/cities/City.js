75c81871f059c989dc562b79a975f42d452084b4
jashah46 <jatin.shah01@gmail.com>
2021-01-19 14:09:21 -0600

    Added city html info

---
+++ scripts/Japan/cities/City.js
@@ -0,0 +1,13 @@
+export const City = (city) => {
+    return `
+    
+        
+            <article class="cities countryArticles">
+                <h2>Popular Cities</h2>
+                <ul>
+                    <li class="cities countryArticles">${city.name}</li>
+                    <li class="cities countryArticles">${city.name}</li>
+                    <li class="cities countryArticles">${city.name}</li>
+                </ul>
+                `
+}
