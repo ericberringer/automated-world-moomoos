74c0878d8d1abb86da674bc16eb88b2192af7eb0
jashah46 <jatin.shah01@gmail.com>
2021-01-20 11:11:50 -0600

    Resolved reference and call errors

--- scripts/Japan/main.js
+++ scripts/Japan/main.js
@@ -2,8 +2,13 @@ import {useCity} from './cities/CityDataProvider.js'
 import {cityList} from './cities/CityList.js'
 
 import {useLandmark} from './locations/LandmarkDataProvider.js'
+import {landmarkList} from './locations/LandmarkList.js'
+import {celebrityList} from './celebrities/CelebritiesList.js'
+
 
 
 // useCity()
 cityList()
+landmarkList()
+celebrityList()
 // console.log(cityList())
