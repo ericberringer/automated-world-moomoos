7cc9a882793d9cc996aac7cfb31b9c2a907e36e4
jashah46 <jatin.shah01@gmail.com>
2021-01-19 22:56:21 -0600

    Added 3 celebreties

---
+++ scripts/Japan/celebrities/CelebritiesDataProvider.js
@@ -0,0 +1,18 @@
+const celebrityCollection = [
+    {
+        image: "",
+        name: "Yoko Ono"
+    },
+    {
+        image: "",
+        name: "Ichiro Suzuki"
+    },
+    {
+        image: "",
+        name: "Marie Kondo"
+    }
+]
+
+export const useCelebrity = () => {
+    return celebrityCollection.slice()
+}
