7ebe6ca9e40d4e9447137fe51022bd732ad2a4d3
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-01-20 09:27:38 -0600

    Added Celebrity Data Provider info

---
+++ scripts/Netherlands/celebrities/CelebritiesDataProvider.js
@@ -0,0 +1,20 @@
+const celebrityCollection = [
+    {
+        image: "",
+        name: "Trijnte Oosterhuis: Music"
+    },
+    {
+        image:"https://www.thefamouspeople.com/profiles/thumbs/frank-rijkaard-4.jpg",
+        name: "Frank Rijkaard: Sports"
+    },
+    {
+        image: "",
+        name: "Williem-Alexander and Maxima: Royalty"
+    }
+]
+
+export const useCelebrity = () => {
+    return celebrityCollection.slice()
+}
+
+// FINISH LOADING PICS
