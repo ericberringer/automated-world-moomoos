697b96515e79b8962333770bc146e7a1a1cbeaa8
jashah46 <jatin.shah01@gmail.com>
2021-01-19 23:08:57 -0600

    Added export data to celebrity

---
+++ scripts/Japan/celebrities/Celebrities.js
@@ -0,0 +1,6 @@
+export const Celebrity = (celebrity) => {
+    return `
+    <img class="countyFactsImage" src="${celebrity.image}">
+        <h3 class="famousCitizens countryArticles">${celebrity.name}</h3>
+        `
+}
